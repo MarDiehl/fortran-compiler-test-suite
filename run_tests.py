ed74f324215e1075e69d38dffb96c141be161062
Brad Richardson <everythingfunctional@protonmail.com>
2023-04-13 11:27:23 -0500

    feat: start compiling and executing code

--- run_tests.py
+++ run_tests.py
@@ -6,6 +6,8 @@ Usage:
 """
 
 import argparse
+import os
+import re
 
 parser = argparse.ArgumentParser(
     prog="run_tests.py",
@@ -14,4 +16,13 @@ parser = argparse.ArgumentParser(
 parser.add_argument('compiler', type=str, nargs=1)
 parser.add_argument('default-options', type=str, nargs=1)
 
-print("hello")
+for root, dirs, files in os.walk("."):
+    fortran_files = list(filter(lambda f: re.match(".*\.[fF][a-zA-Z0-9]*$", f), files))
+    if any(fortran_files): # Are any files Fortran?
+        print("Found fortran in: ", root, " they are: ", fortran_files)
+        for file in fortran_files:
+            orig_path = os.curdir
+            exe_name = os.path.splitext(file)[0] + ".exe"
+            os.chdir(root)
+            os.system("gfortran {0} -o {1} && ./{1}".format(file, exe_name))
+            os.chdir(orig_path)
