05bf50a3bc5c80fea6846b034e6a6b72b1abfb23
Brad Richardson <everythingfunctional@protonmail.com>
2024-01-23 17:43:25 -0600

    feat: start checking outputs

--- framework/checkers.py
+++ framework/checkers.py
@@ -4,13 +4,20 @@ Classes to perform checks of test case outcomes
 
 from framework.execution_result import ExecutionResult, NormalTermination
 from framework.test_result import Check
+import re
 
 class NormalTerminate:
+    def __init__(self, stdout : str, stderr : str, files : {str : str}):
+        self.stdout = stdout
+        self.stderr = stderr
+        self.files = files
+
     def check(self, outcome : ExecutionResult):
         if isinstance(outcome.outcome, NormalTermination):
-            return [Check("Successful execution", True)]
+            basic_check = [Check("Successful execution", True)]
         else:
-            return [Check("{outcome}, but expected normal termination".format(outcome = outcome.outcome), False)]
+            basic_check = [Check("{outcome}, but expected normal termination".format(outcome = outcome.outcome), False)]
+        return basic_check + check_screen_outputs(self.stdout, self.stderr, outcome)
 
 class ErrorTerminate:
     def check(self, outcome : ExecutionResult):
@@ -30,8 +37,26 @@ def create_checker(expected):
             return CompileOnly()
         else:
             if (expected.get("normal_termination")):
-                return NormalTerminate()
+                return NormalTerminate(
+                    expected.get("stdout", ""),
+                    expected.get("stderr", ""),
+                    expected.get("output_files", {})
+                )
             else:
                 return ErrorTerminate()
     else:
         return FailToCompile()
+
+def check_screen_outputs(exp_stdout, exp_stderr, outcome):
+    checks = []
+    if exp_stdout != "":
+        if re.match(exp_stdout, outcome.stdout):
+            checks.append(Check("found '{0}' in stdout".format(exp_stdout), True))
+        else:
+            checks.append(Check("Did not find '{0}' in stdout".format(exp_stdout), False))
+    if exp_stderr != "":
+        if re.match(exp_stderr, outcome.stderr):
+            checks.append(Check("found '{0}' in stderr".format(exp_stderr), True))
+        else:
+            checks.append(Check("Did not find '{0}' in stderr".format(exp_stderr), False))
+    return checks
